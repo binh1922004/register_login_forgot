eb26329ef879d97689a97e6d567829fb5f05745f
binh1922004 <binh1920042018@gmail.com>
2024-09-17 23:35:50 +0700

    them bootstrap va chinh sua trang login

--- src/main/java/ball/controller/home/AdminController.java
+++ src/main/java/ball/controller/home/AdminController.java
@@ -13,9 +13,7 @@ import java.io.IOException;
 public class AdminController extends HttpServlet {
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                String username = (String) req.getAttribute("username");
-//                String roleid = (String) req.getAttribute("roleid");
-                System.out.println(username);
+
                 req.getRequestDispatcher("/view/home.jsp").forward(req, resp);
         }
 }
